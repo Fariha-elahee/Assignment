1fd7a945e2ba23b2063e42c28100a8fc44d6b1e1
Fariha-elahee <elaheef25@gmail.com>
2021-07-15 01:51:34 +0600

    final

--- Question1.cpp
+++ Question1.cpp
@@ -1,26 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-
 int Triplets(int a[], int n, int sum)
 {
-
     int result=0;
-
-
     for (int i = 0; i < n-2; i++)
     {
 
-       for (int j = i+1; j < n-1; j++)
-       {
-
-           for (int k = j+1; k < n; k++)
+       for (int j = i+1; j < n-1; j++){
+        for (int k = j+1; k < n; k++)
                if (a[i] + a[j] + a[k] < sum)
                    result++;
        }
     }
-
     return result;
 }
 
